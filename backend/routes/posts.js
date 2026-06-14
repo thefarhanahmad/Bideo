24bd835cd4b07124c9394b40b0397eb9de55e035
farhan <akhtarfarhan281@gmail.com>
2026-06-14 19:02:18 +0530

    icons and delete post

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createPost, getPosts, getFollowedPosts, togglePostLike, getPost } = require('../controllers/post');
+const { createPost, getPosts, getFollowedPosts, togglePostLike, getPost, updatePost, deletePost } = require('../controllers/post');
 const { protect } = require('../middlewares/auth');
 const upload = require('../middlewares/multer');
 
@@ -9,6 +9,8 @@ router.get('/', getPosts);
 router.get('/followed', protect, getFollowedPosts);
 router.get('/:id', getPost);
 router.post('/', protect, upload.single('image'), createPost);
+router.put('/:id', protect, upload.single('image'), updatePost);
+router.delete('/:id', protect, deletePost);
 router.post('/:id/like', protect, togglePostLike);
 
 module.exports = router;
