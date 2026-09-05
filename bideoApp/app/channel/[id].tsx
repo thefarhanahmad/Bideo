1e25c37cb84ed5a4106fac0a17e4a093d383a54c
farhan <akhtarfarhan281@gmail.com>
2026-09-05 16:26:19 +0530

    feat: align channel share URL to /c/:id and expand Android deep link intent filters

--- bideoApp/app/channel/[id].tsx
+++ bideoApp/app/channel/[id].tsx
@@ -96,7 +96,7 @@ export default function ChannelScreen() {
   const handleShare = async () => {
     if (!channel) return;
     try {
-      const url = `https://bideo.in/channel/${channel._id}`;
+      const url = `https://bideo.in/c/${channel._id}`;
       await Share.share({
         message: `Check out ${channel.channelName || channel.name} on Bideo!\n${url}`,
         url: url,
